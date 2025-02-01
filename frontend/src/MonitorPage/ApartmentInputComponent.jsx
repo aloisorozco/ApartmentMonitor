258f17a857e7017b2fe9065a8f63c00382d92962
aloisorozco <alois.orozco@gmail.com>
2025-02-01 12:21:35 -0500

    added func for main page

--- frontend/src/MonitorPage/ApartmentInputComponent.jsx
+++ frontend/src/MonitorPage/ApartmentInputComponent.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 
-export default function ApartmentInputComponent() {
+import {isEmpty} from "lodash";
+
+export default function ApartmentInputComponent(props) {
+  const [url, setUrl] = useState("");
+  const {apartmentListings, setApartmentListings} = props;
+
+  const handleAdd = () => {
+    if (isEmpty(url)) {
+      return;
+    }
+    setApartmentListings([...apartmentListings, {
+      title: url,
+      url: url,
+      targetPrice: 600,
+      currentPrice: 900
+    }])
+    setUrl("")
+  }
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <TextField
@@ -11,11 +29,16 @@ export default function ApartmentInputComponent() {
         variant="outlined"
         fullWidth
         margin="normal"
+        value={url}
+        onChange={(e) => {
+          setUrl(e.target.value)
+        }}
       />
       <Button
         variant="contained"
         color="primary"
-        type="submit"
+        disabled={isEmpty(url)}
+        onClick={handleAdd}
       >
         Add to watchlist
       </Button>
