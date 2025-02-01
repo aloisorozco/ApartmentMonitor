c38304ed765c5d52189de0825d51f5dc291b832d
Daniel Grimut <grimut.daniel@gmail.com>
2025-02-01 13:46:18 -0500

    basic opps done - not tested

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import hashlib
+import uuid
 from flask_cors import CORS
 import firebase_admin
 import time
@@ -7,9 +8,6 @@ from firebase_admin import credentials, firestore
 
 class Server():
     
-    def hash_data(data):
-        return hashlib.sha256(data.encode()).hexdigest()
-
     # letting flask know that all stuff it needs is in this dir
     app = Flask(__name__)
     CORS(app)
@@ -18,24 +16,69 @@ class Server():
         self.cred: credentials.Certificate = cred
         firebase_admin.initialize_app(cred)
         self.db = firestore.client()
-       
+    
+    def hash_data(data):
+        return hashlib.sha256(data.encode()).hexdigest()
+
+    # todo
+    @app.route('/db_api/send_msgd', methods=['GET'])
+    def send_msgd(self):
+        pass
+
+    # todo
+    @app.route('/db_api/send_email', methods=['GET'])
+    def send_email(self):
+        pass
+
+    # todo
+    @app.route('/db_api/update_listing', methods=['GET'])
+    def update_listing(self):
+        pass
+        
     @app.route('/db_api/fetch_watchlist', methods=['GET'])
     def fetch_watchlist(self):
         email = request.form.get('email')
-        email_hash = hash_data(email)
+        email_hash = self.hash_data(email)
+        wtachlist = self.db.collection('users').document(email_hash).collection('watchlist').get()
+
+        if wtachlist.exists:
+            return jsonify(wtachlist.to_dict())
+        else:
+            return jsonify({})
+        
 
     @app.route('/db_api/save_listing', methods=['POST'])
     def save_listing(self):
-        fname = request.form.get('fname')
-        lname = request.form.get('lname')
-        pword = request.form.get('password')
+        email = request.form.get('fname')
+        email_hash = self.hash_data(email)
 
+        price_curr = request.form.get('curr_price')
+        pirce_target = request.form.get('target_price')
+        desc = request.form.get('desc')
+
+        listing_id = uuid.uuid4()
+
+        watchlist_ref = self.db.collection('users').document(email_hash).collection('watchlist')
+        watchlist_ref.document(listing_id).set({
+            "addedAt": time.time()
+        })
+
+        listing_docref = self.db.collection('apartments').document(listing_id)
+        listing_docref.set({
+            "price": price_curr,
+            "price_target": pirce_target,
+            "description": desc
+        })
 
     @app.route('/db_api/remove_listing', methods=['DELETE'])
     def remove_listing(self):
-        fname = request.form.get('fname')
-        lname = request.form.get('lname')
-        pword = request.form.get('password')
+        email = request.form.get('fname')
+        email_hash = self.hash_data(email)
+        listing_id = request.form.get('listing_id')
+
+        watchlist_ref = self.db.collection('users').document(email_hash).collection('watchlist')
+        watchlist_ref.document(listing_id).delete()
+        self.db.collection('apartments').document(listing_id).delete()
 
     @app.route('/db_api/register_user', methods=['POST'])
     def register_user(self):
@@ -70,19 +113,23 @@ class Server():
 
 
     @app.route('/db_api/auth_user', methods=['GET'])
-    def auth_user(self, password, email):
+    def auth_user(self):
+        password = request.form.get('password')
+        email = request.form.get('email')
+
         enterred_password = self.hash_data(password)
         enterred_email = self.hash_data(email)
         user_db = self.db.collection("users").document(enterred_email).get()
 
         if not user_db.exists:
-            print("User does not exist or provided information is wrong")
-            return False
+            response = jsonify({"error": "User does not exist or provided information is wrong"})
+            response.status_code = 400
+            return response
         
         user_info = user_db.to_dict()
         password_db = user_info.get(password, "There was an error")
-        if(enterred_password == password_db):
-            return True
-        else:
-            print("Password is invalid")
-            return False
+        if(enterred_password != password_db):
+            response = jsonify({"error": "Password Invalid"})
+            response.status_code = 400
+            return response
+
