003bd9d34ae79b3da9cf5129bc3f330de50b18b4
Daniel Grimut <grimut.daniel@gmail.com>
2025-02-01 13:10:55 -0500

    progress

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import hashlib
 from flask_cors import CORS
 import firebase_admin
+import time
 from firebase_admin import credentials, firestore
 
 class Server():
@@ -20,15 +21,21 @@ class Server():
        
     @app.route('/db_api/fetch_watchlist', methods=['GET'])
     def fetch_watchlist(self):
-        pass
+        email = request.form.get('email')
+        email_hash = hash_data(email)
 
     @app.route('/db_api/save_listing', methods=['POST'])
     def save_listing(self):
-        pass
+        fname = request.form.get('fname')
+        lname = request.form.get('lname')
+        pword = request.form.get('password')
+
 
     @app.route('/db_api/remove_listing', methods=['DELETE'])
     def remove_listing(self):
-        pass
+        fname = request.form.get('fname')
+        lname = request.form.get('lname')
+        pword = request.form.get('password')
 
     @app.route('/db_api/register_user', methods=['POST'])
     def register_user(self):
@@ -42,12 +49,24 @@ class Server():
         password_encr = self.hash_data(pword)
 
         # make sure email is not in the firebase db based on hash here
-        email_ref = self.db.collection('users')
+        email_docref = self.db.collection('users').document(email_encr)
 
+        doc = email_docref.get()
 
         # Check here if email already exists
-        pass
-
+        if doc.exists:
+            response = jsonify({"error": "fuck you, your account already exists"})
+            response.status_code = 400
+            return response
+        
+        print("saving user")
+        email_docref.set({
+            "fname": fname,
+            "lname": lname,
+            "email": email,
+            "password_hashed": password_encr,
+            "createdAt": time.time()
+        })
 
 
     @app.route('/db_api/auth_user', methods=['GET'])
