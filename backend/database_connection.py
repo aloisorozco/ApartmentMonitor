ad52d1db7457bddd7d897ddd1a1b34f111eb1eb5
Alessio Cipriano-Kardous <48657408+Greatone3145@users.noreply.github.com>
2025-02-01 12:38:10 -0500

    Database functionality
    - Added tables
    - Added subtables
    - Added relationship

--- backend/database_connection.py
+++ backend/database_connection.py
@@ -2,12 +2,34 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-# Use a service account.
-cred = credentials.Certificate('C:\\Users\\cszty\\Downloads\\cred.json')
+# Use a service account
+cred = credentials.Certificate('C:\\Users\\Alessio\\Downloads\\cred.json')
 
+# Initialize DB
 app = firebase_admin.initialize_app(cred)
 
+# Connect DB
 db = firestore.client()
 
-doc_ref = db.collection("users").document("alovelace")
-doc_ref.set({"first": "Ada", "last": "Lovelace", "born": 1815})
+# Setup tables
+user_table = db.collection('users').document('userID')
+apartment_table = db.collection('apartments').document('listingID')
+
+# Set user data
+user_table.set({
+    "username": "",
+    "email": "",
+    "password": "",
+    "createdAt": ""
+})
+
+apartment_table.set({
+    "price": "", "location": "", 
+    "description": "", 
+    "ownerID": user_table
+})
+
+watchlist_sub = user_table.collection('watchlist').document('dummy_apartment_ID')
+watchlist_sub.set({
+    "addedAt": firestore.SERVER_TIMESTAMP
+})
