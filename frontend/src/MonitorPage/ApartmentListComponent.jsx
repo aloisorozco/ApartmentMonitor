8502523de6e8646ae36b2e5a47b791050a0c13e6
Alois Orozco <57361077+aloisorozco@users.noreply.github.com>
2025-02-01 12:21:59 -0500

    added func for main page (#4)

--- frontend/src/MonitorPage/ApartmentListComponent.jsx
+++ frontend/src/MonitorPage/ApartmentListComponent.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
-import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Link from '@mui/material/Link';
 
-export default function ApartmentListComponent() {
+import {isNil} from "lodash";
+
+export default function ApartmentListComponent(props) {
+  const {apartmentListings} = props;
+
+  if (isNil(apartmentListings) || apartmentListings.length === 0) {
+    return null;
+  }
+  
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      
-    </Box>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Title</TableCell>
+            <TableCell align="right">Target Price</TableCell>
+            <TableCell align="right"> Current Price</TableCell>
+            <TableCell align="right">URL</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {apartmentListings.map((apartmentListing) => (
+            <TableRow
+              key={apartmentListing.title}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {apartmentListing.title}
+              </TableCell>
+              <TableCell align="right">{apartmentListing.targetPrice}</TableCell>
+              <TableCell align="right">{apartmentListing.currentPrice}</TableCell>
+              <TableCell align="right"><Link href={apartmentListing.url}>Listing</Link></TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
