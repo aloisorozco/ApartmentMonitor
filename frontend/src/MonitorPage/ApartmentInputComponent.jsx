23a74e78b76c6ddfe41dbf97721ea8235a288c4c
Nicholas Pop <94801567+Cocovert1@users.noreply.github.com>
2025-02-04 20:47:02 -0500

    Frontend login/logout session (#14)
    * bug fixes
    
    * password Fix
    
    * Fixed watchlist fetch
    
    * Update Login.jsx
    
    * form to JSON
    
    * Register post
    
    * Register and login
    
    * get_json for json post
    
    * login
    
    * login
    
    * fixed proxy rotation
    
    multithreaded proxy rotation  and webscraping works
    
    * authFIX
    
    * Global authentication
    
    create a custom hook for global authentication tracking across pages
    
    * scraper + proxi rotation
    
    proxies do NOT work - they suck so not using them for now
    
    * refreshing listings
    
    listing refresh - not done
    
    * Added login/logout status and cleaned up page
    
    using sessionStorage we store the authStatus of the user
    
    Cleaned up header component and App css to look and fit page size
    
    * Delete sign in
    
    * after register, navigates to login
    
    ---------
    
    Co-authored-by: Daniel Grimut <grimut.daniel@gmail.com>
    Co-authored-by: B004A <nem4tim@gmail.com>

--- frontend/src/MonitorPage/ApartmentInputComponent.jsx
+++ frontend/src/MonitorPage/ApartmentInputComponent.jsx
@@ -2,44 +2,38 @@ import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-
-import {isEmpty} from "lodash";
+import { isEmpty } from 'lodash';
 
 export default function ApartmentInputComponent(props) {
-  const [url, setUrl] = useState("");
-  const {apartmentListings, setApartmentListings} = props;
+  const [url, setUrl] = useState('');
+  const { apartmentListings, setApartmentListings } = props;
 
   const handleAdd = () => {
     if (isEmpty(url)) {
       return;
     }
-    setApartmentListings([...apartmentListings, {
-      title: url,
-      url: url,
-      targetPrice: 600,
-      currentPrice: 900
-    }])
-    setUrl("")
-  }
+    setApartmentListings([
+      ...apartmentListings,
+      {
+        title: url,
+        url: url,
+        targetPrice: 600,
+        currentPrice: 900,
+      },
+    ]);
+    setUrl('');
+  };
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ width: '100%', padding: 2 }}>
       <TextField
         label="Apartment URL"
         variant="outlined"
         fullWidth
-        margin="normal"
         value={url}
-        onChange={(e) => {
-          setUrl(e.target.value)
-        }}
+        onChange={(e) => setUrl(e.target.value)}
       />
-      <Button
-        variant="contained"
-        color="primary"
-        disabled={isEmpty(url)}
-        onClick={handleAdd}
-      >
+      <Button variant="contained" color="primary" disabled={isEmpty(url)} onClick={handleAdd} sx={{ mt: 1 }}>
         Add to watchlist
       </Button>
     </Box>
