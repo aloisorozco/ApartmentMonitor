a53f5f667fe369767f294c33e2ee58e112951cf8
Daniel Grimut <grimut.daniel@gmail.com>
2025-02-05 23:31:03 -0500

    proxie rotation aiohttp
    v1 of proxy rotation with aiogttp and asyncio - SSL issues, but advancing somehwre, need to dig abit more to see if proxy issue or SSL issue. We are now scraping new proxies from the proxy list webiste every time we want to webscrape.

--- backend/proxy_scraper.py
+++ backend/proxy_scraper.py
@@ -16,14 +16,18 @@ class ProxyScraper:
       result = requests.get(ProxyScraper._PROXIES_URL, headers=self.headers, timeout=3)
       doc = BeautifulSoup(result.text, "html.parser")
 
-      table = doc.find("table", {"class" : "table table-striped table-bordered"})
+      table = doc.find("table", {"class" : "table table-striped table-bordered"}).find_all("tr")[1:] # firs thing is a header we dont need
+      proxies = []
 
-      return {
-        "table": table,
-      }
-    except:
-      print("Error occurred while scraping url")
+      for row in table:
+        ip_info = row.find_all("td")
+        ip = ip_info[0].string
+        port = int(ip_info[1].string)
+        proxies.append((ip, port))
 
+        # we can extract other things like location and stuff - for now we just keep this
 
-ps = ProxyScraper()
-print(ps.proxy_scraping())
+      return proxies
+    except Exception as e:
+      print(f'Error occurred while scraping url: {e}')
+      return []
--- backend/web_scraper.py
+++ backend/web_scraper.py
@@ -1,55 +1,73 @@
 from bs4 import BeautifulSoup
 import requests
-import concurrent.futures
+from proxy_scraper import ProxyScraper
+import aiohttp
+import asyncio
 
 class WebScraper:
   
-  TESTURL = "https://www.kijiji.ca/v-apartments-condos/winnipeg/2br-suite-in-character-building-in-the-heart-of-downtown/1700547336"
+  _TESTURL = "https://www.kijiji.ca/v-apartments-condos/winnipeg/2br-suite-in-character-building-in-the-heart-of-downtown/1700547336"
+  _HEADERS = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/117.0"}
+  _PROXY_SCRAPER = ProxyScraper()
 
-  def __init__(self):
-    self.headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/117.0"
-    }
-
-    # with open("proxies.txt", 'r') as f:
-    #   proxies = f.read().split("\n")
-    #   # we are using ThreadPoolExecutor because the bottleneck are the network call opps
-    #   with concurrent.futures.ThreadPoolExecutor() as executor:
-    #     executor.map(self.webscrapeKijijiPage, proxies)
-
-
-  # def webscrapeKijijiPage_iprotation(self, proxy):
+  def websrcape_url(self):
     
-  #   print(f'Proxy used: {proxy}')
-      
-  #   try:  
-  #     result = requests.get(WebScraper.TESTURL, headers=self.headers, proxies={"http": proxy, "https": proxy}, timeout=3)
-  #     doc = BeautifulSoup(result.text, "html.parser")
-
-  #     return {
-  #       "url": WebScraper.TESTURL,
-  #       "title": doc.find("h1", {"itemprop" : "name"}).string,
-  #       "price": doc.find("span", {"itemprop" : "price"}).get("content")
-  #     }
-  #   except:
-  #     print("Error occurred while scraping url")
-  
-  # print("No proxies worked - I am going to loose it")
-        
-  def webscrapeKijijiPage(self, url):
-      
+    proxies = WebScraper._PROXY_SCRAPER.proxy_scraping()
     try:  
-      result = requests.get(url, headers=self.headers, timeout=3)
+      
+      result = asyncio.run(WebScraper._fetch_listing_data(proxies, WebScraper._TESTURL))
+      if result == None:
+         print(f'Proxies failed - could not get any result')
+         return
+      
       doc = BeautifulSoup(result.text, "html.parser")
 
       return {
-        "url": WebScraper.TESTURL,
+        "url": WebScraper._TESTURL,
         "title": doc.find("h1", {"itemprop" : "name"}).string,
         "price": doc.find("span", {"itemprop" : "price"}).get("content")
       }
-    except:
-      print("Error occurred while scraping url")
+    except Exception as e:
+      print(f'Error occurred while scraping url: {e}')
+  
+  async def _fetch_listing_data(proxies, url):
+    # disabling SSL for now
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+      tasks = [asyncio.create_task(WebScraper._try_with_proxy(url, f'http://{proxy[0]}:{proxy[1]}', session)) for proxy in proxies]
 
-url = "https://www.kijiji.ca/v-apartments-condos/winnipeg/2br-suite-in-character-building-in-the-heart-of-downtown/1700547336"
+      while tasks:
+        done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+
+        res = None
+        # getting the first proxy that resolved sucesfully
+        for task in done:
+          try:
+              # break if we have a proxy hit
+              res = task.result()
+              break
+          except Exception as e:
+              print(f"Task failed: {e}")
+
+        # go over this again, and wait for whatever was already pending
+        # asyncio.wait does not rescehule pending task on a subsequent .wait() call if we pass it the pending tasks
+        tasks = list(pending)
+
+      # Cancel all tasks if we have one that is successfull
+      for task in pending:
+        task.cancel()
+
+      await asyncio.gather(*pending, return_exceptions=True)  
+      return res
+
+  async def _try_with_proxy(url, proxy, session):
+      print(f'Using Proxy {proxy}')
+      async with session.get(url, proxy=proxy, headers=WebScraper._HEADERS, timeout=aiohttp.ClientTimeout(total=3.0)) as response:
+          if response.status != 200:
+             raise Exception(f'failed to scrape with proxy {proxy}')
+          else:
+            return await response
+
+
 ws = WebScraper()
-print(ws.webscrapeKijijiPage(url))
+ws.websrcape_url()
+      
