5916b25afb2337b93ce0d0c8672898a04cb8e118
YuriTarded12 <57156001+YuriTarded12@users.noreply.github.com>
2025-03-09 19:08:09 -0400

    Optimized UpdateListing and scheduled jobs

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, current_app
 import hashlib
 import uuid
 from flask_cors import CORS
@@ -13,9 +13,10 @@ import json
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 
-
+# collection = db entry that contains nothing more than documents
+# document = "end point" that contains properties and other documents/collections
 class Server():
-    
+
     # letting flask know that all stuff it needs is in this dir
     app = Flask(__name__)
     CORS(app)
@@ -27,9 +28,9 @@ class Server():
 
     with open("email_cred.json") as f:
         data = json.load(f)
-        email_address=data['email_address']
-        email_password=data['email_password']
-        
+        email_address = data['email_address']
+        email_password = data['email_password']
+
     ws = None
 
     def __init__(self) -> None:
@@ -38,9 +39,46 @@ class Server():
     scheduler = BackgroundScheduler()
     scheduler.start()
 
-    @scheduler.scheduled_job(IntervalTrigger(seconds=5))
-    def begin_scraping():
-        print("Chance implement this")
+    @scheduler.scheduled_job(IntervalTrigger(minutes=1))
+    def scheduled_scraping():
+        print("Beginning scheduled job")
+        with Server.app.app_context():
+
+
+            users_ref = Server.db.collection('users')
+            users_snap = users_ref.stream()
+
+            for user_snap in users_snap:
+                # print("User: "+user_snap.get("email"))
+                email = user_snap.get("email")
+
+                watchlist_ref = Server.db.collection('users').document(user_snap.id).collection('watchlist')
+                watchlist_snap = watchlist_ref.stream()
+
+                if watchlist_snap:
+                    for watchlist_item in watchlist_snap:
+                        apartment_id = watchlist_item.id
+
+                        apartment_ref = Server.db.collection("apartments").document(apartment_id)
+                        apartment_snap = apartment_ref.get()
+                        if apartment_snap.exists:
+                            # print("\tHas apartment: " 
+                            #         + apartment_snap.get("description") + " in their watchlist")
+                            url = apartment_snap.get("url")
+                            if url:
+                                listing_data = Server.ws.websrcape_url_premium_proxies(url)
+                                price_curr = float(listing_data.get("price"))
+                                if price_curr != float(apartment_snap.get("price")):
+
+                                    print(f"User {email} has apartment needing of update: {apartment_snap}")
+
+                                    with Server.app.test_client() as client:
+                                        response = client.get(f"/db_api/update_listing?listing_id={apartment_id}&price={price_curr}&email={email}")
+                                        print(f"Internal API call response: {response.status_code}, {response.get_json()}")
+
+                        
+                
+
 
     def hash_data(data):
         return hashlib.sha256(data.encode()).hexdigest()
@@ -48,8 +86,8 @@ class Server():
     @app.route('/db_api/send_email', methods=['GET'])
     def send_email(receiver_email, subject, body):
         smtp_server = "smtp.gmail.com"
-        smtp_port = 587  
-        
+        smtp_port = 587
+
         msg = MIMEMultipart()
         msg["From"] = Server.email_address
         msg["To"] = receiver_email
@@ -57,73 +95,91 @@ class Server():
 
         msg.attach(MIMEText(body, "plain"))
 
-
         mail_server = smtplib.SMTP(smtp_server, smtp_port)
         mail_server.starttls()  # Upgrade to a secure connection
         mail_server.login(Server.email_address, Server.email_password)  # Login
-        mail_server.sendmail(Server.email_address, receiver_email, msg.as_string())  # Send email
+        mail_server.sendmail(Server.email_address,
+                             receiver_email, msg.as_string())  # Send email
 
         response = jsonify({})
         response.status_code = 200 
         return response
-
-    # todo TO TEST
-    @app.route('/db_api/update_listing', methods=['GET'])
-    def update_listing(listing_id, price):
-        apartment_ref = Server.db.collection("apartments").document(listing_id)
-        apartment_ref.update({"price" : price})
-        print("Sent mail to "+receiver_email)
-
-        response = jsonify({})
-        response.status_code = 200
-        return response
     
+
     
-    # TODO: TO TEST
     @app.route('/db_api/update_listing', methods=['GET'])
-    def update_listing(listing_id, price):
-        # Query all users
-        users_ref = Server.db.collection('users')
-        users_snap = users_ref.stream()
+    def update_listing():
+
+        listing_id = ""
+        price = 0.0
+        email = ""
+
+        try:
+            listing_id = request.args.get('listing_id')
+            price = float(request.args.get('price'))
+            email = request.args.get("email")
+
+            if len(listing_id) == 0:
+                raise Exception("listing ID is empty")
+            if price == 0.0:
+                raise Exception("Price is 0")
+            if len(email) == 0:
+                raise Exception("Email is empty")
+        except ValueError as e:
+            print(f'[ERROR] error when updating listing: {e}')
+            response = jsonify({})
+            response.status_code = 500
+            return response
+        
+        # this is caused when user input is invalid
+        except Exception as e:
+            print(f'[ERROR] error when updating listing: {e}')
+            response = jsonify({})
+            response.status_code = 400
+            return response
+
+
         apartment_ref = Server.db.collection("apartments").document(listing_id)
+
         apartment_snap = apartment_ref.get()
-        if apartment_snap.exists:
-            if price <= int(apartment_snap.get("price_target")):
-                # print("Apartment "+str(listing_id)+" has dropped in price to "+str(price)+" which is below the target price of "+str(apartment_snap.get("price_target")))
-                # Iterate over each user
-                for user_doc in users_snap:
-                    user_id = user_doc.id
-                    # Query the user's watchlist to see if the apartment is there
-                    watchlist_ref = Server.db.collection('users').document(user_id).collection('watchlist')
-                    watchlist_snap = watchlist_ref.stream()
 
-                    for watchlist_item in watchlist_snap:
-                        apartment_id = watchlist_item.id
-                        if apartment_id == listing_id:
-                            print("Apartment "+str(listing_id)+" found in watchlist of user "+str(user_id))
-                            subject = "Price Drop Alert!"
-                            body = "The appartment: "+str(apartment_snap.get("description")+" has dropped below target price!")
-                            Server.send_email(user_doc.get("email"), subject, body)
+        if price < apartment_snap.get("price_target"):
+            subject = "Price Drop Alert!"
+            body = "The apartment: "+str(apartment_snap.get("description")+" has dropped below target price!")
+            Server.send_email(email, subject, body)
 
-        apartment_ref.update({"price" : price})
 
+        apartment_ref.update({"price_target" : price})
         response = jsonify({"ok": "item updated"})
         response.status_code = 200
         return response
+       
         
     @app.route('/db_api/fetch_watchlist', methods=['GET'])
     def fetch_watchlist():
-        data = request.get_json()
-        email = data.get('email')
+        email =  request.args.get('email')
         email_hash = Server.hash_data(email)
         watchlist = Server.db.collection('users').document(email_hash).collection('watchlist').get()
         response = jsonify({})
         response.status_code = 200
 
-        if watchlist.exists:
-            response =  jsonify(watchlist.to_dict())
-        
+        if len(watchlist) > 0:
+            listings = []
+            # querry each listing info - each listing is a DocumentSnapshot object from user - we need to requery each one
+            # TODO: see if we did not mess up in our db design here, seems wierd we need to requerry so much
+            for listing in watchlist:
+                listing_id = listing.id 
+                # .document(listing_id) will get OR create document with listing_id -> returns a document referance
+                # doing .get() on the document referance -> gets the document snapshot itself; need to check if object itself exists in db or not
+                listing_doc = Server.db.collection('apartments').document(listing_id).get()
+                if listing_doc.exists:
+                    listings.append(listing_doc.to_dict())
+
+                
+            response = jsonify({"listings" : listings})
+
         return response
+
 
     @app.route('/db_api/save_listing', methods=['POST'])
     def save_listing():
@@ -131,40 +187,44 @@ class Server():
         email = data.get('email')
         email_hash = Server.hash_data(email)
 
-        price_target = data.get('target_price')
+        price_target = float(data.get('target_price'))
         url = data.get('url')
 
         try:
-        # Web scrape
+            # Web scrape
             listing_data = Server.ws.websrcape_url_premium_proxies(url)
-            price_curr = listing_data.get('price')
+            price_curr = float(listing_data.get('price'))
             desc = listing_data.get('title')
             location = listing_data.get('location')
-            image_link = listing_data.get('image_link')
-        
+            image_link = listing_data.get('images')
+
         except Exception as e:
             print(f'[ERROR] error when parsing: {e}')
-            response = jsonify({'error' : 'parsing issue'})
+            response = jsonify({'error': 'parsing issue'})
             response.status_code = 500
             return response
+
+
+        listing_id = str(uuid.uuid4())
 
         listing_data_processed = {
             "price": price_curr,
             "price_target": price_target,
-            "location" : location,
-            "description" : desc,
-            "image_link" : image_link,
-            "url" : url
+            "location": location,
+            "description": desc,
+            "image_link": image_link,
+            "url": url,
+            "listing_id" : listing_id
         }
 
-        listing_id = str(uuid.uuid4())
-
-        watchlist_ref = Server.db.collection('users').document(email_hash).collection('watchlist')
+        watchlist_ref = Server.db.collection('users').document(
+            email_hash).collection('watchlist')
         watchlist_ref.document(listing_id).set({
             "addedAt": time.time()
         })
 
-        listing_docref = Server.db.collection('apartments').document(listing_id)
+        listing_docref = Server.db.collection(
+            'apartments').document(listing_id)
         listing_docref.set(listing_data_processed)
 
         response = jsonify(listing_data_processed)
@@ -173,11 +233,11 @@ class Server():
 
     @app.route('/db_api/remove_listing', methods=['DELETE'])
     def remove_listing():
-        data = request.get_json()
-        email = data.get('fname')
+        email = request.args.get('email')
+        listing_id = request.args.get('listing_id')
         email_hash = Server.hash_data(email)
-        listing_id = data.get('listing_id')
 
+        # TODO: check that listing exists before removing it lol
         watchlist_ref = Server.db.collection('users').document(email_hash).collection('watchlist')
         watchlist_ref.document(listing_id).delete()
         Server.db.collection('apartments').document(listing_id).delete()
@@ -195,7 +255,7 @@ class Server():
 
         # use the email as unqiue ID
         email = data.get('email')
-        email_encr = Server.hash_data(email) 
+        email_encr = Server.hash_data(email)
         password_encr = Server.hash_data(pword)
 
         # make sure email is not in the firebase db based on hash here
@@ -208,7 +268,7 @@ class Server():
             response = jsonify({"error": "Account already exists"})
             response.status_code = 400
             return response
-        
+
         print("saving user")
         email_docref.set({
             "fname": fname,
@@ -222,25 +282,24 @@ class Server():
         response.status_code = 200
         return response
 
-
     @app.route('/db_api/auth_user', methods=['POST'])
     def auth_user():
-        data = request.get_json()
-        password = data.get('password')
-        email = data.get('email')
+        password = request.json.get('password')
+        email = request.json.get('email')
 
         enterred_password = Server.hash_data(password)
         enterred_email = Server.hash_data(email)
         user_db = Server.db.collection("users").document(enterred_email).get()
-        
+
         if not user_db.exists:
-            response = jsonify({"error": "User does not exist or provided information is wrong"})
+            response = jsonify(
+                {"error": "User does not exist or provided information is wrong"})
             response.status_code = 400
             return response
-        
+
         user_info = user_db.to_dict()
         password_db = user_info.get('password_hashed', "There was an error")
-        if(enterred_password != password_db):
+        if (enterred_password != password_db):
             response = jsonify({"error": "Password Invalid"})
             response.status_code = 400
             return response
