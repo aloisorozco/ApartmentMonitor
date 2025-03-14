82aff6b29585dd48012bb77e339bffdc8572ae0e
Daniel Grimut <57418761+D-grimut@users.noreply.github.com>
2025-03-13 21:11:53 -0400

    Multithreading (#24)
    * support for concurency
    
    added some support for concurency:
    -locking the singleton instance at initilaisation (first acess to singleton class)
    - refreshing proxies after 100 concurent requests
    - error handeling in case of blocking timout when acess proxy list
    - tested, but need more testing to make sure that concurent requests are not causing any wierd issues
    
    * reverted-selenium
    
    reverted the slenium implementation. selenium way too slow.
    
    * merged alois changes
    
    * tested multithreading with new changes
    
    * flask routes refactor
    
    refactore the flask routes, the routes are now set in a centralized way, wrapped inside the Server object.
    
    * updated quotes (issues for deployment)

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,7 @@
+from server import app
 from server import Server
 
 if __name__ == "__main__":
     HOST = "0.0.0.0"
     PORT = "5500"
-
-    server = Server()
-    server.app.run(host=HOST, port=PORT)
-    # Server.update_listing("c9b6a4fc-0967-43a6-8bc9-0510c3482b5e", 0)
+    app.run(host=HOST, port=PORT, threaded=True)
