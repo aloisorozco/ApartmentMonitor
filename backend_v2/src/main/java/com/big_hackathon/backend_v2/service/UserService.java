f66e73b104b82c6be819c68d64d050dcfb87fe24
aloisorozco <57361077+aloisorozco@users.noreply.github.com>
2025-03-17 20:46:29 -0400

    added skeleton for migration
    TODO:
    -we need to explore how to integrate firebase with spring (i saw this link on stackoverflow maybe its a good starting point: https://firebase.google.com/docs/database/admin/start)
    -i think proxy/webscraping should stay on python. email notifs too maybe

---
+++ backend_v2/src/main/java/com/big_hackathon/backend_v2/service/UserService.java
@@ -0,0 +1,28 @@
+package com.big_hackathon.backend_v2.service;
+
+import com.big_hackathon.backend_v2.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public String listUsers() {
+        return "TODO";
+    }
+
+    public String getUser(Long id) {
+        return "TODO";
+    }
+
+    public String insertUser(User user) {
+        return "TODO";
+    }
+
+    public String updateUser(User user, Long id) {
+        return "TODO";
+    }
+
+    public String deleteUser(Long id) {
+        return "TODO";
+    }
+}
