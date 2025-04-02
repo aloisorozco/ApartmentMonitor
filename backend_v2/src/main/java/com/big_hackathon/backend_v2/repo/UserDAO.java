bdbf29dfe54d0a1af29180f8e06a8be2445aa660
Daniel Grimut <grimut.daniel@gmail.com>
2025-04-01 21:43:15 -0400

    Extended Chance Code for mock CRUD
     - Connecting to Firestore
    - Mock CRUD to fetch users to have a template of the syntax of interaction with Firestore.

--- backend_v2/src/main/java/com/big_hackathon/backend_v2/repo/UserDAO.java
+++ backend_v2/src/main/java/com/big_hackathon/backend_v2/repo/UserDAO.java
@@ -1,9 +1,27 @@
 package com.big_hackathon.backend_v2.repo;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+
+import lombok.SneakyThrows;
 
 @Repository
 public class UserDAO {
+
+    private final Firestore db;
+
+    // No need to use Autowired - Spring injects automatically for constructors.
+    public UserDAO(Firestore db){
+        this.db = db;
+    }
+
     public void saveUser(){
         // TODO
     }
@@ -18,5 +36,18 @@ public class UserDAO {
     }
     public void fetchWatchlist(){
         // TODO
+    }
+
+    // TODO: Research and figure out how Sneaky Throws works
+    @SneakyThrows
+    public void getUser(){
+
+        // TODO: Research firestore syntax + Caveats
+        ApiFuture<QuerySnapshot> query = db.collection("users").get();
+        QuerySnapshot querySnapshot = query.get();
+        List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
+        for (QueryDocumentSnapshot document : documents) {
+            System.out.println("User: " + document.getId());
+        }
     }
 }
