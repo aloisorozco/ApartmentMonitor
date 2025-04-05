8074ef21fffc83dacc38f3e6b293168e31232265
Daniel Grimut <grimut.daniel@gmail.com>
2025-04-04 23:55:03 -0400

    CRUD used started
    - started CRUD for user
    - documented syntax to make it easier to understand the Firestore java SDK

--- backend_v2/src/main/java/com/big_hackathon/backend_v2/repo/UserDAO.java
+++ backend_v2/src/main/java/com/big_hackathon/backend_v2/repo/UserDAO.java
@@ -2,20 +2,26 @@ package com.big_hackathon.backend_v2.repo;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.firestore.WriteResult;
 
 import lombok.SneakyThrows;
+
+// collection = db entry that contains nothing more than documents
+// document = "end point" that contains properties and other documents/collections
 
 @Repository
 public class UserDAO {
 
     private final Firestore db;
+    private final Logger logger = LoggerFactory.getLogger(UserDAO.class);
 
     // No need to use Autowired - Spring injects automatically for constructors.
     public UserDAO(Firestore db){
@@ -25,8 +31,14 @@ public class UserDAO {
     public void saveUser(){
         // TODO
     }
-    public void delUser(){
-        // TODO
+
+    @SneakyThrows
+    public void delUser(String userID){
+        ApiFuture<WriteResult> res = db.collection("users").document("userID").delete();
+
+        // TODO: this will throw an error in the event the .get() fails - we should handle this in error middleware
+        WriteResult metadata = res.get();
+        logger.info("Deleted user " +  userID + " at time " + metadata.getUpdateTime());
     }
     public void authUser(){
         // TODO
@@ -34,20 +46,29 @@ public class UserDAO {
     public void updateUser(){
         // TODO
     }
-    public void fetchWatchlist(){
-        // TODO
+
+    @SneakyThrows
+    public void fetchWatchlist(String userID, String email){
+        // ApiFuture<QuerySnapshot> query = db.collection("users").get()
     }
 
-    // TODO: Research and figure out how Sneaky Throws works
     @SneakyThrows
     public void getUser(){
 
-        // TODO: Research firestore syntax + Caveats
+        // .get() initiates an async read request to the db 
         ApiFuture<QuerySnapshot> query = db.collection("users").get();
-        QuerySnapshot querySnapshot = query.get();
+
+        // we have another .get() here since we need to wait for the async API futuure read request
+        // this get() here is BLOCKING - we are waiting until db resolves
+        QuerySnapshot querySnapshot = query.get(); // QuerySnapshot = snapshot of docs in the collection
         List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
         for (QueryDocumentSnapshot document : documents) {
             System.out.println("User: " + document.getId());
         }
+    }
+
+    private String hashData(){
+        // TODO: implement hashing function - we could use guava from google as a dependency
+        return null;
     }
 }
